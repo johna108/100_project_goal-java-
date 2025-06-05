5d961beb3025597349162058a6fc6d39c3845ee2
johna108 <151762489+johna108@users.noreply.github.com>
2025-06-05 14:40:13 +0530

    Create Bill Splitter.java
    Bill splitter

---
+++ Bill Splitter.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("welcome to BillSplitter");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("What was the final bill amount: ");
+        int a = sc.nextInt();
+        System.out.print("Number of people: ");
+        int b = sc.nextInt();
+        int splitter = a/b;
+        System.out.println("Each should pay: "+splitter +"$");
+
+    }
+}
