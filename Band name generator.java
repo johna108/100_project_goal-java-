e4cb9b52a1403eed21ffef16ac67f964be163d86
johna108 <151762489+johna108@users.noreply.github.com>
2025-06-05 14:25:32 +0530

    Create Band name generator.java
    Band generator

---
+++ Band name generator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("welcome to band name generator");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("What is the city that you grew in: ");
+        
+        String city_name = sc.nextLine();
+        System.out.print("Name of Your first pet: ");
+        String Pet_name = sc.nextLine();
+        System.out.println("Your new band name is " + city_name +" " + Pet_name);
+    }
+}
